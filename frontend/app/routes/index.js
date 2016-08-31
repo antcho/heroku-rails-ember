a6863c2fd72ad3108642b11da96f1a99c0922618
antcho <antcho60@gmail.com>
2016-08-31 17:16:26 +0200

    route problem corrected

--- frontend/app/routes/index.js
+++ frontend/app/routes/index.js
@@ -7,7 +7,7 @@ export default Ember.Route.extend({
 	//
 	ajax: Ember.inject.service(),
 	model() {
-		return this.get('ajax').request(`${config.host}/friends`).then(function(data){
+		return this.get('ajax').request(`/friends`).then(function(data){
 			return {
 				friendsCount: data.data.length
 			};
